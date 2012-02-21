7a58403abb8b35fd89c33375fa31af0b120f2f2c
Théophile Helleboid - chtitux <chtitux@gmail.com>
2012-02-21 20:28:58 +0100

    Update Activity grid

--- gen/net/helleboid/tangloid/R.java
+++ gen/net/helleboid/tangloid/R.java
@@ -12,15 +12,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int tangloid=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f050009;
+        public static final int button1=0x7f050008;
         public static final int editText1=0x7f050002;
         public static final int editText2=0x7f050003;
         public static final int editText3=0x7f050004;
         public static final int editText4=0x7f050006;
-        public static final int include1=0x7f050007;
-        public static final int layout_grid_gridLinearLayout=0x7f050008;
+        public static final int layout_grid_gridLinearLayout=0x7f050007;
         public static final int tableLayout1=0x7f050000;
         public static final int tableRow1=0x7f050001;
         public static final int tableRow2=0x7f050005;
